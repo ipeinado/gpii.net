4a3739f49be865521b06278fd86f91f5f40bd7f0
Ben Caldwell <bcaldwell@pushing7.com>
2016-06-23 10:01:02 -0500

    Modified gulp file to include Bootstrap SCSS recommendations for specificity and autoprefixing.

--- web/sites/all/themes/devspace/gulpfile.js
+++ web/sites/all/themes/devspace/gulpfile.js
@@ -2,19 +2,22 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     watch = require('gulp-watch'),
     sass = require('gulp-sass'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer');
     livereload = require('livereload');
+
 
     gulp.task('default', ['watch']);
 
     var input = './sass/**/*.scss';
-    var output = './css';
+    var output = './tmpcss';
 
     gulp.task('sass', function () {
       return gulp
         // Find all `.scss` files from the `stylesheets/` folder
         .src(input)
         // Run Sass on those files
-        .pipe(sass())
+        .pipe(sass({precision: '8'}))
         // Write the resulting CSS in the output folder
         .pipe(gulp.dest(output));
     });
@@ -23,12 +26,30 @@ var gulp = require('gulp'),
       return gulp
         // Watch the input folder for change,
         // and run `sass` task when something happens
-        .watch(input, ['sass'])
+        .watch(input, ['sass', 'tmpcss'])
         // When there is a change,
         // log a message in the console
         .on('change', function(event) {
           console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
         });
+    });
+
+    gulp.task('tmpcss', function () {
+      var processors = [
+        autoprefixer({browsers: [
+          "Android 2.3",
+          "Android >= 4",
+          "Chrome >= 20",
+          "Firefox >= 24",
+          "Explorer >= 8",
+          "iOS >= 6",
+          "Opera >= 12",
+          "Safari >= 6"
+        ]}),
+      ];
+      return gulp.src('./tmpcss/*.css')
+        .pipe(postcss(processors))
+        .pipe(gulp.dest('./css'));
     });
 
     // if working on a remote server, use by ssh port forwarding -L 35729:localhost:35729 p7sbcadocsweb@qual-msn-stg06
