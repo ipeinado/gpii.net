d1534d481ab808bc135f47c97003cda2730c6d47
ipeinado <i.peinado.martinez@gmail.com>
2016-10-27 09:43:17 +0200

    first changes in navbar

--- web/sites/all/themes/devspace/templates/system/page.tpl.php
+++ web/sites/all/themes/devspace/templates/system/page.tpl.php
@@ -75,7 +75,7 @@
 ?>
 <div id="wrap" class="wrapper">
   <header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
-    <div class="container">
+    <div class="container-fluid">
       <div class="navbar-header">
         <?php if ($logo): ?>
           <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
