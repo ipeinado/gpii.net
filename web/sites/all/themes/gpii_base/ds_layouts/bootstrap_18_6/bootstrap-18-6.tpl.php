b01c85e96e8894a9f6013d21aa1ed51705ffe849
Ben Caldwell <bcaldwell@pushing7.com>
2017-01-05 13:51:53 -0600

    Initial files for gpii_devspace theme.

---
+++ web/sites/all/themes/gpii_base/ds_layouts/bootstrap_18_6/bootstrap-18-6.tpl.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @file
+ * Bootstrap 8-16 template for Display Suite.
+ *
+ * Available variables:
+ *
+ * Layout:
+ * - $classes: String of classes that can be used to style this layout.
+ * - $contextual_links: Renderable array of contextual links.
+ * - $layout_wrapper: wrapper surrounding the layout.
+ *
+ * BBC: Overridden to invoke fluid behavior when left or right content is empty.
+ */
+?>
+
+
+<<?php print $layout_wrapper; print $layout_attributes; ?> class="<?php print $classes; ?>">
+  <?php if (isset($title_suffix['contextual_links'])): ?>
+    <?php print render($title_suffix['contextual_links']); ?>
+  <?php endif; ?>
+  <?php if ($left || $right): ?>
+  <div class="row">
+  <?php if ($left): ?>
+    <<?php print $left_wrapper; ?> class="<?php if (empty($right)): ?>col-sm-24 <?php else: ?>col-sm-18 <?php endif; ?><?php print $left_classes; ?>">
+      <?php print $left; ?>
+    </<?php print $left_wrapper; ?>>
+  <?php endif; ?>
+  <?php if ($right): ?>
+    <<?php print $right_wrapper; ?> class="<?php if (empty($left)): ?>col-sm-24 <?php else: ?>col-sm-6 <?php endif; ?><?php print $right_classes; ?>">
+      <?php print $right; ?>
+    </<?php print $right_wrapper; ?>>
+  </div>
+  <?php endif; ?>
+  <?php endif; ?>
+</<?php print $layout_wrapper ?>>
+
+
+<?php if (!empty($drupal_render_children)): ?>
+  <?php print $drupal_render_children ?>
+<?php endif; ?>
---
+++ web/sites/all/themes/gpii_devspace/gulpfile.js
@@ -0,0 +1,62 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    watch = require('gulp-watch'),
+    sass = require('gulp-sass'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    livereload = require('livereload'),
+    sourcemaps = require('gulp-sourcemaps'),
+    scsslint = require('gulp-scss-lint');
+
+
+    gulp.task('default', ['watch']);
+
+    var input = './scss/**/*.scss';
+    var output = './tmpcss';
+
+    gulp.task('sass', function () {
+      return gulp
+        // Find all `.scss` files from the `stylesheets/` folder
+        .src(input)
+        // Run Sass on those files
+        .pipe(sass({precision: '8'}).on('error', sass.logError))
+        // Write the resulting CSS in the output folder
+        .pipe(gulp.dest(output));
+    });
+
+    gulp.task('watch', function() {
+      return gulp
+        // Watch the input folder for change,
+        // and run `sass` task when something happens
+        .watch(input, ['sass', 'tmpcss'])
+        // When there is a change,
+        // log a message in the console
+        .on('change', function(event) {
+          console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+        });
+    });
+
+    gulp.task('tmpcss', function () {
+      var processors = [
+        autoprefixer({browsers: [
+          "Android 2.3",
+          "Android >= 4",
+          "Chrome >= 20",
+          "Firefox >= 24",
+          "Explorer >= 8",
+          "iOS >= 6",
+          "Opera >= 12",
+          "Safari >= 6"
+        ]}),
+      ];
+      return gulp.src('./tmpcss/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss(processors))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./css'));
+    });
+
+    // if working on a remote server, use by ssh port forwarding -L 35729:localhost:35729 username@server
+
+    server = livereload.createServer();
+    server.watch([__dirname + "/js", __dirname + "/css"]);
