000db635cce9c223d06abcf12087007342b9f2ce
Ben Caldwell <bcaldwell@pushing7.com>
2017-03-20 16:11:01 -0500

    - Composer vendor directory cleanup. - Basic search just-in-time translation - CSS fixes related to Feed3

--- web/sites/vendor/composer/autoload_namespaces.php
+++ web/sites/vendor/composer/autoload_namespaces.php
@@ -6,8 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Psr\\Log\\' => array($vendorDir . '/psr/log'),
-    'JakubOnderka\\PhpConsoleHighlighter' => array($vendorDir . '/jakub-onderka/php-console-highlighter/src'),
-    'JakubOnderka\\PhpConsoleColor' => array($vendorDir . '/jakub-onderka/php-console-color/src'),
-    'Boris' => array($vendorDir . '/d11wtq/boris/lib'),
+    'Rize\\UriTemplate' => array($vendorDir . '/rize/uri-template/src'),
 );
