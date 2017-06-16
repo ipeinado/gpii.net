fce1551b6a876561ad6ad9cbbdc79fb878a0809e
Ben Caldwell <bcaldwell@pushing7.com>
2017-06-16 15:41:02 -0500

    Adjusted bootstrap so that book navigation outline button wasn't screwed up.

--- web/sites/all/themes/bootstrap/templates/book/book-navigation.tpl.php
+++ web/sites/all/themes/bootstrap/templates/book/book-navigation.tpl.php
@@ -33,7 +33,9 @@
 ?>
 <?php if ($tree || $has_links): ?>
   <div id="book-navigation-<?php print $book_id; ?>" class="book-navigation">
-    <?php print $tree; ?>
+    <?php // BBC: Commented out as this is creating some strange output
+          // see https://www.drupal.org/node/1697570
+          // print $tree; ?>
 
     <?php if ($has_links): ?>
     <ul class="pager clearfix">
