e02e8d89fa1198dc53c3f8e1a68217f109cd1f90
Ben Caldwell <bcaldwell@pushing7.com>
2017-11-01 08:29:22 -0500

    Progress/status update for browse virtual stores search.

---
+++ web/sites/all/modules/gpii_saa_custom/js/browse-category.js
@@ -0,0 +1,16 @@
+/*jslint browser: true*/
+/*global jQuery*/
+
+(function ($) {
+  'use strict';
+
+  $(document).ready(function(){
+    $('.grid').masonry({
+      itemSelector: '.grid-item',
+      columnWidth: '.grid-sizer',
+      percentPosition: true,
+      gutter: 10
+    });
+  });
+
+}(jQuery));
