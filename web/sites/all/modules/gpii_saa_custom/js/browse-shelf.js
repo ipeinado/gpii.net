c7e4fa4ea3502cf5ef0636a32d7f1b84652eaf1e
Ben Caldwell <bcaldwell@pushing7.com>
2017-11-03 15:34:04 -0500

    shelf focus features

--- web/sites/all/modules/gpii_saa_custom/js/browse-shelf.js
+++ web/sites/all/modules/gpii_saa_custom/js/browse-shelf.js
@@ -25,12 +25,26 @@
   //increase the clicktarget on the flexslider items
   $(document).ready(function(){
     //console.log($('.slides > li'));
-    $('.slides').on('click', function() {
+    $('.slides li').on('click', function() {
       window.location.href = $(this)
         .find('a[href]')
         .first()
         .attr('href');
+    });
+    // Call the event handler on #text
+    $('.slides li').hover(function(){
+      // add a class to children so we can style on hover/focus
+      $(this).children().addClass("focused");
+    },
+      // Event two mouse out remove class
+      function(){
+      $(this).children().removeClass("focused");
+    });
 
+    $('.slides li a').focus(function() {
+      $(this).closest('div').addClass('focused');
+    }).blur(function() {
+      $(this).closest('div').removeClass('focused');
     });
   });
 
