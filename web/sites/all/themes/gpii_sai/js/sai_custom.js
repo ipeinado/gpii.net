d3bc77e1d3b2d9d51678c93c1b40b654bb394f45
Ben Caldwell <bcaldwell@pushing7.com>
2017-11-20 12:34:13 -0600

    page-front template fix and home page search blocks clicktarget js.

--- web/sites/all/themes/gpii_sai/js/sai_custom.js
+++ web/sites/all/themes/gpii_sai/js/sai_custom.js
@@ -4,6 +4,15 @@
 (function ($) {
   'use strict';
 
-
+  // Tag the body when viewed in IE to fix some style issues.
+  $(document).ready(function(){
+    $('.highlighted .panel')
+    .on('click', function() {
+      window.location.href = $(this)
+        .find('.content a[href]')
+        .first()
+        .attr('href');
+    });
+  });
 
 }(jQuery));
