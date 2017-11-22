b5712c3c368574cf5a255b52af91e09b8388bbfe
Ben Caldwell <bcaldwell@pushing7.com>
2017-11-22 08:24:02 -0600

    Fixed bug with missing skiptranslate class on home page.

--- web/sites/all/themes/gpii_base/templates/menu/menu-tree.func.php
+++ web/sites/all/themes/gpii_base/templates/menu/menu-tree.func.php
@@ -13,7 +13,7 @@ function gpii_base_menu_tree__primary(&$variables) {
   // This is the same as the implementation of the Drupal Bootstrap theme, but
   // it adds the "primary" class.
   //
-  // It also introduces a "skiptranslate" class that tells Google Translate
+  // It also introduces a "skiptranslate" class that tells Google Translate to skip this menu item
 
 
   // get a list of installed languages
@@ -22,11 +22,12 @@ function gpii_base_menu_tree__primary(&$variables) {
   $currlanguage = substr($_SERVER['REQUEST_URI'], 1);
   // reduce the string to the first three characters
   $currlanguage = substr($currlanguage, 0, 3);
+  kpr($currlanguage);
 
   // initialize a variable in case we need to tell Google Translate to skip this menu
   $skiptranslate = '';
-  // if the first three characters include a slash, then this
-  if(strstr($currlanguage, '/')) {
+  // if the first three characters include a slash or  only two characters long (es, de, el), then add skiptranslate
+  if(strstr($currlanguage, '/') || (strlen($currlanguage) == 2)) {
     $currlanguage = substr($currlanguage, 0, 2);
     if (in_array($currlanguage,$languages)) {
       $skiptranslate = ' skiptranslate';
