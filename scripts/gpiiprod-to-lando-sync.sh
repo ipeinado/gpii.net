e06bf241fdd178cee9e3e8eee64ebb8901b60e4e
Ben Caldwell <bcaldwell@qualtim.com>
2018-11-07 15:52:13 -0600

    WIP: Revision Category Changes and theme updates for n-tree power search

--- scripts/gpiiprod-to-lando-sync.sh
+++ scripts/gpiiprod-to-lando-sync.sh
@@ -3,7 +3,7 @@
 # exit 0
 # cd <profile/version controlled directory>
 
-DRUSH_PATH="/home/bcaldwell/code/gpii.net/web/vendor/bin/drush"
+DRUSH_PATH="drush"
 
 echo "Starting..."
 echo "Grabbing latest info from Git..."
@@ -11,7 +11,6 @@ echo "Grabbing latest info from Git..."
 cd ~/code/gpii.net/web
 
 git pull
-
 
 # switch to the right starting directory
 cd ~/code/gpii.net/web/sites/default
@@ -26,7 +25,7 @@ echo "syncing database from @gpiiprod"
 $DRUSH_PATH @gpiiprod sql-dump --result-file=/var/www/clients/client4/web5/tmp/gpiiprod-db.sql
 
 #rsync the file created above so that it is available locally
-rsync -e 'ssh ' -akz --remove-source-files gpiiweb@192.168.123.79:/var/www/clients/client4/web5/tmp/gpiiprod-db.sql /home/bcaldwell/code/gpii.net/backups/gpiiprod-db.sql
+rsync -e 'ssh ' -akz --remove-source-files gpiiweb@192.168.123.79:/var/www/clients/client4/web5/tmp/gpiiprod-db.sql /home/ben/code/gpii.net/backups/gpiiprod-db.sql
 
 # # drop the current DB and import the new
 lando db-import ../../../backups/gpiiprod-db.sql
@@ -34,16 +33,16 @@ lando db-import ../../../backups/gpiiprod-db.sql
 echo "Database successfully imported!"
 
 # # disable CSS/JS aggregation
-# lando drush -y config-set system.performance css.preprocess 0
+lando drush -y config-set system.performance css.preprocess 0
 
 # # Turning on maint. mode.
 # lando drush --yes vset maintenance_mode 1
 
 # # disable modules
-# lando drush --yes dis googleanalytics
+lando drush --yes dis googleanalytics
 
 # # enable modules
-# lando drush --yes en reroute_email search_api_override patchinfo
+lando drush --yes en reroute_email search_api_override patchinfo stage_file_proxy
 
 # # adjust variables
 # lando drush vset --yes file_private_path '' # set to null to avoid errors in local environments (ex. /var/www/clients/client3/web18/private)
