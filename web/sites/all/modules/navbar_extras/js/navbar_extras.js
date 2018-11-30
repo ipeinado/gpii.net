96f8ca04e8bb36eca74611489e93ec6630c00e2f
Ben Caldwell <bcaldwell@qualtim.com>
2018-11-30 11:54:18 -0600

    module updates: file_entity, navbar_extras, paypal_payment, webform, xmlsitemap

--- web/sites/all/modules/navbar_extras/js/navbar_extras.js
+++ web/sites/all/modules/navbar_extras/js/navbar_extras.js
@@ -6,12 +6,12 @@
 (function($) {
   Drupal.behaviors.navbar_extras = {
     attach: function(context, settings) {
-      if (settings.navbar_extras.navbar_open_menu) {
+      if (settings.navbar_extras && settings.navbar_extras.navbar_open_menu) {
         var a = "navbar-item--2";
         localStorage.setItem('Drupal.navbar.activeTabID', '"navbar-item--2"');
         localStorage.setItem('Drupal.navbar.activeTab', '"navbar-item--2"');
       }
-      if (settings.navbar_extras.navbar_lock_vertically) {
+      if (settings.navbar_extras && settings.navbar_extras.navbar_lock_vertically) {
         localStorage.setItem('Drupal.navbar.trayVerticalLocked', "true");
       }
     }
