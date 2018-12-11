ccfe7d85b1a809aa9f7d9df76dbb7a5b29103fa8
Ben Caldwell <bcaldwell@qualtim.com>
2018-12-11 14:54:23 -0600

    Added script to disable save button on edit page until req'd. fields are added

---
+++ web/sites/all/modules/revision_category/js/revision-edit.js
@@ -0,0 +1,25 @@
+(function ($) {
+    'use strict';
+
+    $(document).ready(function(){
+        // expand the summary field by default
+        $('.link-edit-summary:contains(Edit)').trigger('click');
+
+        // disable the save button until required fields are set
+        $('#edit-submit').prop('disabled', true).css('color', '#999999').css('cursor', 'not-allowed');
+
+        $('#edit-log').on('keyup blur', function() {
+            var currentString = $('#edit-log').val();
+            var revisionType = $('#edit-category').children('option:selected').val();
+            if (currentString.length > 2 && revisionType != '' )  {
+                //console.log(revisionType);
+                $('#edit-submit').removeAttr("disabled").css('color', '#5a5a5a').css('cursor', 'pointer');
+            }
+            else {
+                $('#edit-submit').prop('disabled', true).css('color', '#999999').css('cursor', 'not-allowed');
+            }
+        });
+    });
+
+    
+}(jQuery));
