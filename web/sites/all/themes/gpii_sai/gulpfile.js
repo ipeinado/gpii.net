8b8d78647305a647f7597d4979cf3733195fc297
Ben Caldwell <bcaldwell@qualtim.com>
2019-05-03 17:05:41 -0500

    turned of CSS grid in autoprefixer to address IE issues

--- web/sites/all/themes/gpii_sai/gulpfile.js
+++ web/sites/all/themes/gpii_sai/gulpfile.js
@@ -41,12 +41,12 @@ var gulp = require('gulp'),
 
     gulp.task('tmpcss', function () {
       var processors = [
-        autoprefixer({browsers: [
+        autoprefixer({grid: false, browsers: [
           "Android 2.3",
           "Android >= 4",
           "Chrome >= 20",
           "Firefox >= 24",
-          "Explorer >= 8",
+          "Explorer >= 9",
           "iOS >= 6",
           "Opera >= 12",
           "Safari >= 6"
