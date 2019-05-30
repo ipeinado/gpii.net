8918a39302d97ec27410cf05d46f7faca7766c43
Shawn Holmes <sholmes@qualtim.com>
2019-05-30 10:48:36 -0500

    Adding js for discon message field

---
+++ web/sites/all/modules/gpii_saa_custom/js/discontinued_edit.js
@@ -0,0 +1,34 @@
+(function($) {
+  'use strict';
+
+  $(document).ready(function() {
+    var status = $('#edit-field-status-und');
+    var dis_msg_wrapper = $('#edit-field-discontinued-message');
+    var dis_msg_none = $('#edit-field-discontinued-message-und-none');
+
+    setDiscontinuedDisplay();
+
+    status.on('change', function() {
+      setDiscontinuedDisplay();
+      if (status.val() != 2) {
+        dis_msg_none.click();
+      }
+    });
+
+    dis_msg_none.on('click', function() {
+      status.val(1);
+      setDiscontinuedDisplay();
+      alert(
+        'By setting Discontinued Message to none the status has been set to active. If it should be a different status, please set it now.'
+      );
+    });
+
+    function setDiscontinuedDisplay() {
+      if (status.val() == 2) {
+        dis_msg_wrapper.css('display', 'initial');
+      } else {
+        dis_msg_wrapper.css('display', 'none');
+      }
+    }
+  });
+})(jQuery);
