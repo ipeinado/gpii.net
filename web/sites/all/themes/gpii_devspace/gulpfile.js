65a6b2da4df8775bc6a0492a35f9e36a86e695e2
Shawn Holmes <sholmes@qualtim.com>
2019-11-05 12:57:09 -0600

    Adding files for new Unified Sass

--- web/sites/all/themes/gpii_devspace/gulpfile.js
+++ web/sites/all/themes/gpii_devspace/gulpfile.js
@@ -1,64 +1,52 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util'),
-    watch = require('gulp-watch'),
-    sass = require('gulp-sass'),
-    postcss = require('gulp-postcss'),
-    autoprefixer = require('autoprefixer'),
-    livereload = require('gulp-livereload'),
-    sourcemaps = require('gulp-sourcemaps'),
-    scsslint = require('gulp-scss-lint');
+  sass = require('gulp-sass'),
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer'),
+  cssnano = require('cssnano'),
+  sourcemaps = require('gulp-sourcemaps'),
+  browserSync = require('browser-sync').create();
 
+var paths = {
+  styles: {
+    src: './scss/**/*.scss',
+    dest: './css'
+  },
+  scripts: {
+    src: './js/**/*.js'
+  }
+};
 
-    gulp.task('default', ['watch']);
+var proxy = 'dev.ds.gpii.net';
 
-    var input = './scss/**/*.scss';
-    var output = './tmpcss';
+function style() {
+  return gulp
+    .src(paths.styles.src)
+    .pipe(sourcemaps.init())
+    .pipe(sass({ precision: 8 }))
+    .on('error', sass.logError)
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(browserSync.stream());
+}
 
-    gulp.task('sass', function () {
-      return gulp
-        // Find all `.scss` files from the `stylesheets/` folder
-        .src(input)
-        // Run Sass on those files
-        .pipe(sass({precision: '8'}).on('error', sass.logError))
-        // Write the resulting CSS in the output folder
-        .pipe(gulp.dest(output))
-        .pipe(livereload());
-    });
+function reload() {
+  browserSync.reload();
+}
 
-    gulp.task('watch', function() {
-      //livereload.listen();
-      return gulp
-        // Watch the input folder for change,
-        // and run `sass` task when something happens
-        .watch(input, ['sass', 'tmpcss'])
-        // When there is a change,
-        // log a message in the console
-        .on('change', function(event) {
-          console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-        });
-    });
+function watch() {
+  browserSync.init({ proxy: proxy });
+  gulp.watch(paths.styles.src, style);
+  gulp.watch([paths.styles.dest, paths.scripts.src]).on('change', browserSync.reload);
+}
 
-    gulp.task('tmpcss', function () {
-      var processors = [
-        autoprefixer({browsers: [
-          "Android 2.3",
-          "Android >= 4",
-          "Chrome >= 20",
-          "Firefox >= 24",
-          "Explorer >= 8",
-          "iOS >= 6",
-          "Opera >= 12",
-          "Safari >= 6"
-        ]}),
-      ];
-      return gulp.src('./tmpcss/*.css')
-        .pipe(sourcemaps.init())
-        .pipe(postcss(processors))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./css'));
-    });
+// Don't forget to expose the task!
+exports.watch = watch;
+exports.style = style;
 
-    // if working on a remote server, use by ssh port forwarding -L 35729:localhost:35729 username@server
+/*
+ * Specify if tasks run in series or parallel using `gulp.series` and `gulp.parallel`
+ */
+var build = gulp.parallel(style, watch);
 
-    // server = livereload.createServer();
-    // server.watch([__dirname + "/js", __dirname + "/css"]);
+exports.default = build;
